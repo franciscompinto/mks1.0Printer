5f21d23186f3d4d66c2d5c37a5e2c43584bd9b91
Francisco Pinto <franciscomenezes94@ua.pt>
2018-08-03 12:09:14 +0100

    cont calib printer

--- Marlin-1.1.x/Marlin/Configuration.h
+++ Marlin-1.1.x/Marlin/Configuration.h
@@ -529,14 +529,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 110, 100, 400, 1000 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 110, 100, 400, 1200 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 2000, 2000, 500, 500 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -544,7 +544,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 120, 50 }
+#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 120, 1000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -1039,7 +1039,7 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (10*60)
+#define HOMING_FEEDRATE_XY (20*60)
 #define HOMING_FEEDRATE_Z  (2*60)
 
 // @section calibrate
